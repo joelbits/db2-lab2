752223622e810c9a2de212c7789ae8b476b51afd
joelbits <joak.dev@gmail.com>
2018-01-26 19:35:06 +0700

    Added Dropping procedures before defining them

--- lab2_procedures.sql
+++ lab2_procedures.sql
@@ -1,5 +1,5 @@
 -- 3. user_email : Procedure : call user_email(); should give a list with email, fnamn and lnamn for all users in db.
-
+DROP PROCEDURE IF EXISTS user_email;
 DELIMITER //
 CREATE PROCEDURE user_email()
 BEGIN
@@ -9,7 +9,7 @@ END //
 DELIMITER ;
 
 -- 4. add_hobby(hobby) : Adds a hobby to the hobbies table
-
+DROP PROCEDURE IF EXISTS add_hobby;
 DELIMITER //
 CREATE PROCEDURE add_hobby(hobby VARCHAR(30))
 BEGIN
